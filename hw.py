5a63267af6a3a1c04520d649c738a7ffb6b4678c
Majestic Taco <90423367+MajesticTaco@users.noreply.github.com>
2021-09-30 15:44:11 +0300

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,8 +1,14 @@
 import math
 
+print ('Enter the requested inputs to calculate the perpendicular function')
+
 x1 = float(input('Enter the value of X1: '))
 y1 = float(input('Enter the value of Y1: '))
 x2 = float(input('Enter the value of X2: '))
 y2 = float(input('Enter the value of Y2: '))
 x3 = float(input('Enter the value of X3: '))
 y3 = float(input('Enter the value of Y3: '))
+
+m = (y2-y1)/(x2-x1)
+b = y1-m*x1
+
