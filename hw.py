655aa96dd101e11bd28ca9b7714937eb42d277b7
Majestic Taco <90423367+MajesticTaco@users.noreply.github.com>
2021-09-30 15:29:54 +0300

    Create hw.py

---
+++ hw.py
@@ -0,0 +1,8 @@
+import math
+
+x1 = float(input('Enter the value of X1: '))
+y1 = float(input('Enter the value of Y1: '))
+x2 = float(input('Enter the value of X2: '))
+y2 = float(input('Enter the value of Y2: '))
+x3 = float(input('Enter the value of X3: '))
+y3 = float(input('Enter the value of Y3: '))
