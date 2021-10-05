1a117d9082116409d490ce084108a8dfd276e080
Majestic Taco <90423367+MajesticTaco@users.noreply.github.com>
2021-10-05 15:08:59 +0000

    Update hw.py

--- hw.py
+++ hw.py
@@ -12,7 +12,7 @@ y3 = float(input('Enter the value of Y3: '))
 m = (y2-y1)/(x2-x1) # Aprēķina m no y=mx+b
 b = y1-m*x1 # Aprēķina m no y=mx+b
 m1 = -m/m**2 # Aprēķina m perpendikulārajai funkcijai
-b1 = -(m1*x3-y3) # Aprēķina b1 perpendikulārajai funkcijai
+b1 = -(m1*x3-y3) # Aprēķina b perpendikulārajai funkcijai
 
 print ('The input function is y =', m,'x +',b,) # Izprintē ABC funkciju, tas nav obligāti jāliek pabeigtajā darbā
 print ('The perpendicular function is y =', m1,'x +',b1,) # Izprintē perpendikulāro funkciju
