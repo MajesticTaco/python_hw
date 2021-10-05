a88ee5d206a8117b26c01a021b8f29ac5703f654
Majestic Taco <90423367+MajesticTaco@users.noreply.github.com>
2021-10-05 14:55:36 +0000

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,14 +1,18 @@
 import math
 
-print ('Enter the requested inputs to calculate the perpendicular function')
+print ('Enter the requested inputs to calculate the perpendicular function') 
 
-x1 = float(input('Enter the value of X1: '))
+x1 = float(input('Enter the value of X1: ')) # A
 y1 = float(input('Enter the value of Y1: '))
-x2 = float(input('Enter the value of X2: '))
-y2 = float(input('Enter the value of Y2: '))
-x3 = float(input('Enter the value of X3: '))
+x2 = float(input('Enter the value of X2: ')) # B
+y2 = float(input('Enter the value of Y2: ')) 
+x3 = float(input('Enter the value of X3: ')) # C
 y3 = float(input('Enter the value of Y3: '))
 
-m = (y2-y1)/(x2-x1)
-b = y1-m*x1
+m = (y2-y1)/(x2-x1) # Aprēķina m no y=mx+b
+b = y1-m*x1 # Aprēķina m no y=mx+b
+m1 = -m/m**2 # Aprēķina m perpendikulārajai funkcijai
+b1 = -(m1*x3-y3) # Aprēķina b1 perpendikulārajai funkcijai
 
+print ('The input function is y =', m,'x +',b,) # Izprintē ABC funkciju, tas nav obligāti jāliek pabeigtajā darbā
+print ('The perpendicular function is y =', m1,'x +',b1,) # Izprintē perpendikulāro funkciju
